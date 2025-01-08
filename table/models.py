44e55a9b5d0e99ded6a7d98db81ef7e1acf89281
avotaangi <alinaignatova67@gmail.com>
2025-01-09 01:15:34 +0300

    Add model

--- table/models.py
+++ table/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class QueryLog(models.Model):
+    # Поле для текста запроса
+    query_text = models.TextField(verbose_name="Текст запроса")
+
+    # Поле для времени запроса
+    query_time = models.DateTimeField(auto_now_add=True, verbose_name="Время запроса")
+
+    # Поле для ответа в формате строки
+    response_data = models.TextField(verbose_name="Ответ (таблица в строковом формате)")
+
+    class Meta:
+        verbose_name = "Журнал запросов"
+        verbose_name_plural = "Журналы запросов"
+        ordering = ['-query_time']  # Сортировка по времени запроса в порядке убывания
+
+    def __str__(self):
+        return f"Запрос: {self.query_text[:50]}... | Время: {self.query_time}"
